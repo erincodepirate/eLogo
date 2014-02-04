178deb83dbe996f18c9061f3e69af5918d4fe97a
Erin Clark <erin.clark.ix@gmail.com>
2014-02-03 23:10:48 -0600

    Fixed old commandParser to not die upon erroneous input

--- src/com/erinix/elogo/commandParser.java
+++ src/com/erinix/elogo/commandParser.java
@@ -16,55 +16,75 @@ public class commandParser {
 			// i must type in if(currentToken.equals...)
 			// for each command
 			if (currentToken.equals("fd")) {
-				currentToken = commandTokens.nextToken();
-				logoView.fd(Integer.parseInt(currentToken));
+				if (commandTokens.hasMoreTokens()) {
+					currentToken = commandTokens.nextToken();
+					Pattern p = Pattern.compile("\\d*");
+					Matcher m = p.matcher(currentToken);
+					if (m.matches()) {
+						logoView.fd(Integer.parseInt(currentToken));
+					}
+				}
 			}
 			else if (currentToken.equals("rt")) {
-				currentToken = commandTokens.nextToken();
-				logoView.rt(Integer.parseInt(currentToken));
+				if (commandTokens.hasMoreTokens()) {
+					currentToken = commandTokens.nextToken();
+					Pattern p = Pattern.compile("\\d*");
+					Matcher m = p.matcher(currentToken);
+					if (m.matches()) {
+						logoView.rt(Integer.parseInt(currentToken));
+					}
+				}
 			}
 			else if (currentToken.equals("lt")) {
-				currentToken = commandTokens.nextToken();
-				logoView.lt(Integer.parseInt(currentToken));
+				if (commandTokens.hasMoreTokens()) {
+					currentToken = commandTokens.nextToken();
+					Pattern p = Pattern.compile("\\d*");
+					Matcher m = p.matcher(currentToken);
+					if (m.matches()) {
+						logoView.lt(Integer.parseInt(currentToken));
+					}
+				}
 			}
 			// allow both integer values for colors as well
 			// as strings
-			else if (currentToken.equals("color")) {
-				currentToken = commandTokens.nextToken();
-				// check to see if the command is followed
-				// by an integer or a string, easiest done
-				// with a simple pattern matching
-				Pattern p = Pattern.compile("\\d*");
-				Matcher m = p.matcher(currentToken);
-				if (m.matches()) {
-					logoView.color(
-						Integer.parseInt(currentToken));
+/*			else if (currentToken.equals("color")) {
+				if (commandTokens.hasMoreTokens()) {
+					currentToken = commandTokens.nextToken();
+					// check to see if the command is followed
+					// by an integer or a string, easiest done
+					// with a simple pattern matching
+					Pattern p = Pattern.compile("\\d*");
+					Matcher m = p.matcher(currentToken);
+					if (m.matches()) {
+						logoView.color(
+							Integer.parseInt(currentToken));
+					}
+					else {
+						int colorNum = 1; // default to black
+						if (currentToken.equals("white"))
+							colorNum = 0;
+						else if (currentToken.equals("black"))
+							colorNum = 1;
+						else if (currentToken.equals("red"))
+							colorNum = 2;
+						else if (currentToken.equals("green"))
+							colorNum = 3;
+						else if (currentToken.equals("blue"))
+							colorNum = 4;
+						else if (currentToken.equals("yellow"))
+							colorNum = 5;
+						else if (currentToken.equals("orange"))
+							colorNum = 6;
+						else if (currentToken.equals("pink"))
+							colorNum = 7;
+						else if (currentToken.equals("magenta"))
+							colorNum = 8;
+						else if (currentToken.equals("grey"))
+							colorNum = 9;
+						logoView.color(colorNum);
+					}
 				}
-				else {
-					int colorNum = 1; // default to black
-					if (currentToken.equals("white"))
-						colorNum = 0;
-					else if (currentToken.equals("black"))
-						colorNum = 1;
-					else if (currentToken.equals("red"))
-						colorNum = 2;
-					else if (currentToken.equals("green"))
-						colorNum = 3;
-					else if (currentToken.equals("blue"))
-						colorNum = 4;
-					else if (currentToken.equals("yellow"))
-						colorNum = 5;
-					else if (currentToken.equals("orange"))
-						colorNum = 6;
-					else if (currentToken.equals("pink"))
-						colorNum = 7;
-					else if (currentToken.equals("magenta"))
-						colorNum = 8;
-					else if (currentToken.equals("grey"))
-						colorNum = 9;
-					logoView.color(colorNum);
-				}
-			}
+			} */
 //			else if(currentToken.equals("ellipse")) {
 //				currentToken = commandTokens.nextToken();
 //				next = commandTokens.nextToken();
@@ -72,8 +92,14 @@ public class commandParser {
 //					       	Integer.parseInt(next));
 //			}
 			else if(currentToken.equals("circle")) {
-				currentToken = commandTokens.nextToken();
-				logoView.circle(Integer.parseInt(currentToken));
+				if (commandTokens.hasMoreTokens()) {
+					currentToken = commandTokens.nextToken();
+					Pattern p = Pattern.compile("\\d*");
+					Matcher m = p.matcher(currentToken);
+					if (m.matches()) {
+						logoView.circle(Integer.parseInt(currentToken));
+					}
+				}
 			}
 //			else if(currentToken.equals("rect")) {
 //				currentToken = commandTokens.nextToken();
@@ -109,46 +135,60 @@ public class commandParser {
 			// simple variable-less for loop like the original logo's repeat command
 			// syntax is repeat num [commands], should handle nested repeat loops
 			else if(currentToken.equals("repeat")) {
-				int times = Integer.parseInt(commandTokens.nextToken());
-				int left = 0;
-				int right = 0;
-				boolean done = false;
-				String inner = "";
-				while (!done) {
-					currentToken = commandTokens.nextToken();
-					if (currentToken.startsWith("[")) {
-						if (left == 0) {
-							inner = inner + currentToken.substring(1) + " ";
-						}
-						else {
-							inner = inner + currentToken + " ";
-						}
-						left++;
-					}
-					else if (currentToken.endsWith("]")) {
-						// count the number of ]s in token, takes care of nested loops
-						for (int j = 0; j < currentToken.length(); j++) {
-							if (currentToken.charAt(j) == ']')
-								right++;
-						}
-						// if we have reached the point where the command should end, 
-						// cut off the end ] and add it, also signal that we are done
-						if (right == left) {
-							inner = inner + currentToken.substring(0, currentToken.length() - 1);
-							done = true;
-						}
-						else {
-							inner = inner + currentToken + " ";
-						}
-					}
-					else {
-						inner = inner + currentToken + " ";
-					}	
+				if (commandTokens.hasMoreTokens()) {
+					
 				}
-				// do the actual loop, recursively calling the commands inside the loop
-				for (int i = 0; i < times; i++) {
-					parseCommands(new StringTokenizer(inner), logoView);
-				} 
+				currentToken = commandTokens.nextToken();
+				Pattern p = Pattern.compile("\\d*");
+				Matcher m = p.matcher(currentToken);
+				if (m.matches()) {
+					int times = Integer.parseInt(currentToken);
+					int left = 0;
+					int right = 0;
+					boolean opens = false;
+					boolean closes = false;
+					boolean done = false;
+					String inner = "";
+					while (!done && commandTokens.hasMoreTokens()) {
+						currentToken = commandTokens.nextToken();
+						if (currentToken.startsWith("[")) {
+							if (left == 0) {
+								inner = inner + currentToken.substring(1) + " ";
+								opens = true;
+							}
+							else {
+								inner = inner + currentToken + " ";
+							}
+							left++;
+						}
+						else if (currentToken.endsWith("]")) {
+							// count the number of ]s in token, takes care of nested loops
+							for (int j = 0; j < currentToken.length(); j++) {
+								if (currentToken.charAt(j) == ']')
+									right++;
+							}
+							// if we have reached the point where the command should end, 
+							// cut off the end ] and add it, also signal that we are done
+							if (right == left) {
+								inner = inner + currentToken.substring(0, currentToken.length() - 1);
+								done = true;
+								closes = true;
+							}
+							else {
+								inner = inner + currentToken + " ";
+							}
+						}
+						else {
+							inner = inner + currentToken + " ";
+						}	
+					}
+					// do the actual loop, recursively calling the commands inside the loop
+					if (opens && closes) {
+						for (int i = 0; i < times; i++) {
+							parseCommands(new StringTokenizer(inner), logoView);
+						} 	
+					}
+				}
 			}
 		}		
 	}
